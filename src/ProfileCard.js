9a1be8e15156f498f801e0c407776d1ec8864175
tenvega <etorra@gmail.com>
2023-07-10 21:26:42 -0400

    added props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,14 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+function ProfileCard({ title, handle, image }) {
+  //   const title = props.title;
+  //   const handle = props.handle;
+  //const { title, handle } = props;
+  return (
+    <div>
+      <img src={image} alt="pda logo" />
+      <div>Title is {title}</div>
+      <div>Handel is {handle}</div>
+    </div>
+  );
 }
+
 export default ProfileCard;
