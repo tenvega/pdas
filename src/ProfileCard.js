4eddd1ee5db2f1824a353eec80112456473d352e
tenvega <etorra@gmail.com>
2023-07-13 07:46:43 -0400

    added description

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,12 +1,21 @@
-function ProfileCard({ title, handle, image }) {
-  //   const title = props.title;
-  //   const handle = props.handle;
-  //const { title, handle } = props;
+function ProfileCard({ title, handle, image, description }) {
   return (
-    <div>
-      <img src={image} alt="pda logo" />
-      <div>Title is {title}</div>
-      <div>Handel is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div>
+          <p className="content">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
